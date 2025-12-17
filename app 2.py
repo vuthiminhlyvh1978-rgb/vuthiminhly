c1f861dd460100487cce2d06f1969693a01e1222
vuthiminhlyvh1978-rgb <vuthiminhlyvh1978@gmail.com>
2025-12-17 15:47:21 +0700

    Update author information in app 2.py

--- app 2.py
+++ app 2.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Vũ thị Minh Lý - Trường  Tiểu học Vĩnh Hảo - ĐT: 0971134966</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Vũ Thị Minh Lý - Trường  Tiểu học Vĩnh Hảo - ĐT: 0971134966</div>", unsafe_allow_html=True)
